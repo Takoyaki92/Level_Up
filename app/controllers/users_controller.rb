6f1cdcebd1886f89099f60b9204d60bd75f44820
Alexander Chen <alex.chen103@gmail.com>
2021-02-13 14:42:04 +0900

    started on the skils

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,10 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  # This displays all of the user's skills in their PROFILE page.
+  def skills
+    @user = User.find(params[:id])
+    @skills = @user.skills
+  end
 end
