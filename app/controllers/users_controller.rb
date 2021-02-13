d498a23e549cb92108979450ba532f646c3b4f73
Alexander Chen <alex.chen103@gmail.com>
2021-02-13 15:25:37 +0900

    you can view user skills on their profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-  end
-
-  # This displays all of the user's skills in their PROFILE page.
-  def skills
-    @user = User.find(params[:id])
     @skills = @user.skills
   end
 end
