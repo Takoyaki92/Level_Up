9329052f754cf173d3f93189a6a3d2dfbe2a1e91
brandon lindberg <lindberg.scott.brandon@gmail.com>
2021-02-13 15:41:50 +0900

    create method for study sessions controller

--- app/controllers/study_sessions_controller.rb
+++ app/controllers/study_sessions_controller.rb
@@ -2,4 +2,21 @@ class StudySessionsController < ApplicationController
   def new
     @study_session = StudySession.new
   end
+
+  def create
+    @study_session = StudySession.new(study_session_params)
+    @study_session.user = current_user
+    if @study_session.save
+      redirect_to "/users/:id"
+      # Might need to change this later
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def study_session_params
+    params.require(:study_session).permit(:goal, :description, :public, :photo)
+  end
 end
