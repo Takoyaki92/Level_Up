28ec5c94248797777fa315762e54b8cff9480916
brandon lindberg <lindberg.scott.brandon@gmail.com>
2021-02-16 22:03:50 +0900

    timer method implemented

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -27,8 +27,10 @@ import "bootstrap";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
+import { initTimer } from '../components/timer';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  initTimer();
 });
