389981dd59cecb9199abbbcf8f16fc0cd176b797
Alexander Chen <alex.chen103@gmail.com>
2021-02-18 22:21:14 +0900

    added study_sessions#new and #create and #index

--- app/controllers/study_sessions_controller.rb
+++ app/controllers/study_sessions_controller.rb
@@ -5,20 +5,24 @@ class StudySessionsController < ApplicationController
   end
 
   def new
+    @skill = Skill.find(params[:skill_id])
     @study_session = StudySession.new
   end
 
   def create
+    @skill = Skill.find(params[:skill_id])
     @study_session = StudySession.new(study_session_params)
-    @study_session.user = current_user
+    @study_session.skill = @skill
+    @study_session.skill.user = current_user
+    # @study_session.user = current_user
     if @study_session.save
-      redirect_to "/users/:id"
+      redirect_to study_sessions_path
       # Might need to change this later
     else
       render :new
     end
-    @skill = resource
-    super
+    # @skill = resource
+    # super
   end
 
   private
