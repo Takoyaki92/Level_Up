16590ca1da00d843b77e9b72ad76f12bc4002bd6
Miriam Derenbach <miriam.derenbach@outlook.com>
2021-02-19 20:57:37 +0900

    studysesh#new working with and without skill selection prior

--- app/javascript/components/submit.js
+++ app/javascript/components/submit.js
@@ -1,6 +1,10 @@
 const submit= () => {
-  document.getElementById("form1").submit();
-  document.getElementById("form2").submit();
-}
+  const button = document.querySelector("#submit-share");
+
+  button.addEventListener('click', (event) => {
+    document.getElementById("form1").submit();
+    document.getElementById("form2").submit();
+  });
+};
 
 export { submit }
