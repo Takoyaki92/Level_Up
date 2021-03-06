813522ee9ae3ecf74c595382e2b60c34194d825a
Glenn Bryant <glennbryant3@gmail.com>
2021-03-06 16:25:52 +0900

    working on timer fading

--- app/javascript/components/card-fade.js
+++ app/javascript/components/card-fade.js
@@ -1,10 +1,13 @@
 const fade= () => {
   const card = document.getElementById("study_session_goal");
   const select = document.getElementById("card");
+  const timerCard = document.getElementById("time-card");
 
   card.addEventListener('keyup', (event) => {
     if (event.key === 'Enter') {
       select.className = "card-style animate__animated animate__fadeOutUp";
+      timerCard.className = "card-style animate__animated animate__fadeInUp";
+
     }
   })
   
