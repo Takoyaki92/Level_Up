0b044f3b017424b9800afe3039e9cef45d7b1bf6
brandon lindberg <lindberg.scott.brandon@gmail.com>
2021-03-11 22:39:52 +0900

    study session new style update

--- app/javascript/components/card-fade.js
+++ app/javascript/components/card-fade.js
@@ -2,11 +2,14 @@ const fade= () => {
   const card = document.getElementById("study_session_goal");
   const select = document.getElementById("card");
   const timerCard = document.getElementById("time-card");
+  const skillTitle = document.getElementById("new-skill-title");
 
   card.addEventListener('keyup', (event) => {
     if (event.key === 'Enter') {
       select.className = "card-style animate__animated animate__fadeOutUp position";
+      select.style.display = "none";
       timerCard.className = "card-style animate__animated animate__fadeInUp position";
+      skillTitle.className = "animate__animated animate__fadeOutUp text-white new-h2";
     }
   })
   
